f019a39d9f9df4c1a6d2430e789dbb6ce419bc92
Andrew Trigg <andrewt51@hotmail.com>
2016-05-28 11:38:21 +0100

    Modularised errors

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
+var validator = require('validator');
+var errorMsg = require('../errors.json');
 
 
 
-function handler(result,other){
-  if(result === true){
-    var token = jwt.sign({name:"andrew"},'secret');
-    return token
+function checkValid(checkIsTrue,res,type){
+  if(!checkIsTrue){
+    throw errorMsg[type]
   }
-  return
 }
 
 router.get('/',function(req,res){
@@ -19,11 +19,15 @@ router.get('/',function(req,res){
 
 router.post('/add',function(req,res){
   user = new User;
+  checkValid(validator.isEmail(req.body.email),res,1);
   user.set(req.body);
   user.generatePassword(req.body.password)
   .then(function(data){
     data.save()
     res.send(data);
+  })
+  .catch(function(err){
+    res.status(err[0]).send(err[1])
   })
 })
 
@@ -31,15 +35,17 @@ router.post('/login',function(req,res){
   User.where({email:req.body.email})
   .fetch()
   .then(function(user){
-    user.validatePassword(req.body.password, user.attributes.password)
-    .then(handler)
-    .then(function(token){
-      res.send(token)
-    })
+      checkValid(user,res,1);
+      return user && user.validatePassword(req.body.password, user.attributes.password)
+  })
+  .then(function(result){
+    checkValid(result,res,2);
+    var token = jwt.sign({name:"andrew"},'secret');
+    res.send(token)
+  })
+  .catch(function(err){
+    res.status(err[0]).send(err[1])
   })
 })
-
-
-
 
 module.exports = router
